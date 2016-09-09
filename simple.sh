ac3e8105c9716fd7b5c80ab4a8248aa408a298d4
Mark Fernandes <mfernandes61@gmail.com>
2016-09-09 15:22:48 +0100

    Update simple.sh

--- simple.sh
+++ simple.sh
@@ -4,6 +4,6 @@
 
 # simple test script for first Dockerfile
 echo "Creating some output..."
-cat *.txt > test/output/alltext.txt
-ls -alFs > test/output/listing.txt
+cat $APP_HOME/*.txt > $APP_HOME/test/output/alltext.txt
+ls -alFs > $APP_HOME/test/output/listing.txt
 echo "...and we're done!"
